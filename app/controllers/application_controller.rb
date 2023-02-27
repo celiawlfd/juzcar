f3f65a0cefe5128694411daacbacc3e05ac7d358
Seraphine Marie <seraphine.marie@ieseg.fr>
2023-02-27 18:09:05 +0100

    model user with devise done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :attribute])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :attribute])
+  end
+
 end
