e74132a247ae6d653b85bd838c5b5355a673c893
Seraphine Marie <seraphine.marie@ieseg.fr>
2023-02-28 11:24:04 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "houses#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :houses do
+    resources :reservations, only: [:create]
+  end
 end
