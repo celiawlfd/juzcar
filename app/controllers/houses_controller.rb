c6654434e736bb48f3c91795729f62fce49742ad
Thomas Mattelaer mattelaer.thomas@hotmail.com <mattelaer.thomas@hotmail.com>
2023-02-28 12:01:52 +0100

    Action Create and New done on Houses controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,2 +1,21 @@
 class HousesController < ApplicationController
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      redirect_to house_path(@house)
+    else
+      render :new, status: :unprocessable_entity
+
+  end
+
+  private
+
+  def house_params
+    params.require(:house).permit(:name, :address, :price_per_night, :number_of_people, :description)
+  end
 end
