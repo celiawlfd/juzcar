c5f1b88fb80c405213f11568ce5d03925055d281
Celia Wilford <celiawilford@gmail.com>
2023-02-28 12:07:18 +0100

    basic of index & show done

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,6 +1,10 @@
 class HousesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
   end
 end
