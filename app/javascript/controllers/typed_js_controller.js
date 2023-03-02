92ec1bb83b200faf64202e0fd2eada0c1dd31489
Seraphine Marie <seraphine.marie@ieseg.fr>
2023-03-02 16:36:10 +0100

    typed js on tilte

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+
+// Connects to data-controller="typed-js"
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["The first smurf platform !", "Find the smurf house of your dreams"],
+      typeSpeed: 60,
+      loop: true
+    })
+  }
+}
