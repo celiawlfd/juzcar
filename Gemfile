865077096448c2670479e76f4eb6cb368ad2d7f4
Seraphine Marie <seraphine.marie@ieseg.fr>
2023-03-02 15:00:49 +0100

    search bar et greeting

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
+gem "pg_search"
+
 gem "devise"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -3,7 +3,11 @@ class HousesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @houses = House.all
+    if params[:query].present?
+      @houses = House.search_by_name_description_and_address(params[:query])
+    else
+      @houses = House.all
+    end
   end
 
   def show
